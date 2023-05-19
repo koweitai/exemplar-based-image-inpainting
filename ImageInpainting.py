e77c205991bd1e188fb00413f0e2e7a1de08b948
Kowei <b08902080@ntu.edu.tw>
2023-05-19 12:12:34 +0800

    Add Pixel class

--- ImageInpainting.py
+++ ImageInpainting.py
@@ -2,16 +2,39 @@ import cv2
 import numpy as np
 import argparse
 
-def compute_dataterm(point):
-    return
+class Pixel:
+    def __init__(self, value, confidence, data, is_filled, is_fillfront):
+        self.value = value
+        self.confidence = confidence
+        self.data = data
+        self.is_filled = is_filled
+        self.is_fillfront = is_fillfront
 
-def compute_priority(point):
-    return point.confidence * compute_dataterm(point)
+    def compute_dataterm(self):
+        return
+
+    def compute_priority(self):
+        return self.confidence * self.compute_dataterm()
+
+def init_image(img_input):
+    img = np.empty(shape=img_input.shape, dtype = np.dtype(Pixel))
+    for i in range(img_input.shape[0]):
+        for j in range(img_input.shape[1]):
+            pixel = Pixel(img_input[i][j], 1, 0, False, False)
+            img[i][j] = pixel
+    return img
+
+def generate_result_image(img):
+    img_result = np.zeros(img.shape, dtype=np.uint8)
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            img_result[i][j] = img[i][j].value
+    return img_result
 
 def find_maxpriority_patch():
     max_priority = 0
     for point in fillfront:
-        if compute_priority(point) > max_priority:
+        if point.compute_priority() > max_priority:
             max_priority_point = point
 
     return max_priority_point
@@ -21,7 +44,7 @@ def compute_similarity(target_patch, source_patch):
 
 def find_source_patch(target_patch):
     max_similarity = 0
-    for source_patch in image:
+    for source_patch in img:
         if compute_similarity(target_patch, source_patch) > max_similarity:
             max_similarity_patch = source_patch
 
@@ -40,14 +63,19 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--input')
     parser.add_argument('--output')
+    parser.add_argument('--patch_size')
     args = parser.parse_args()
 
     img_input = cv2.imread(args.input, cv2.IMREAD_GRAYSCALE)
-    while not is_fillfront_empty(img_input):
+    img = init_image(img_input)
+    while not is_fillfront_empty(img):
         target_patch = find_maxpriority_patch()
         source_patch = find_source_patch(target_patch)
         copy_imagedata(target_patch, source_patch)
-        update_confidence(img_input)
+        update_fillfromt(img)
+        update_confidence(img)
+    img_output = generate_result_image(img)
+    cv2.imwrite(args.output, img_output)
 
 if __name__ == "__main__":
     main()
