b75f494f919f67ab8051770d371b01e95e8819c0
Kowei <b08902080@ntu.edu.tw>
2023-05-22 16:23:14 +0800

    Add data term calculation

--- ImageInpainting.py
+++ ImageInpainting.py
@@ -2,70 +2,81 @@ import cv2
 import numpy as np
 import argparse
 
+alpha = 255 # normalization factor
+
 # class Image:
 #     def __init__(self, pixels, contour):
 #         self.pixels = pixels
 #         self.contour = contour
 
 class Pixel:
-    def __init__(self, r, c, value, patch, is_filled, is_fillfront):
+    def __init__(self, r, c, value, is_filled, is_fillfront, value_patch):
         self.r = r
         self.c = c
         self.value = value # [B, G, R]
-        self.patch = patch
+        # self.patch = patch
         self.confidence = 0 if is_fillfront else 1
         # self.data = self.compute_data()
         self.is_filled = is_filled # filled or not
         self.is_fillfront = is_fillfront # in the area to be filled
+        self.value_patch = value_patch # 九宮格的value
+
+    def set_patch(self, patch):
+        self.patch = patch
 
     def compute_confidence(self):
         """ Compute confidence of the central pixel of the patch. """
         return self.confidence
     
-    def compute_data(self, contour):
+    def compute_data(self):
         """ Compute the data on linear structures around the pixel. """
-        # normalization factor
-        alpha = 255
-        norm = self.normal_direction(contour)
-
-        gradient_vec = self.gradient_magnitude()
-        data = np.sqrt(norm.dot(gradient_vec)) / alpha
+        norm = self.normal_direction()
+        gradient = self.gradient_vector()
+        data = np.abs(norm.dot(gradient)) / alpha
         return data
 
-    def compute_priority(self, contour):
-        return self.compute_confidence() * self.compute_data(contour)
+    def compute_priority(self):
+        return self.compute_confidence() * self.compute_data()
     
-    def gradient_magnitude(self, k = 2): # per pixel (Sobel)
+    def gradient_vector(self, k = 2): # per pixel (Sobel)
         max_gradient = -1
-        for point in self.patch:
-            gr = ((point.patch[0][2] + k * point.patch[1][2] + point.patch[2][2]) - (point.patch[0][0] + k * point.patch[1][0] + point.patch[2][0])) / (k + 2)
-            gc = ((point.patch[0][0] + k * point.patch[0][1] + point.patch[0][2]) - (point.patch[2][0] + k * point.patch[2][1] + point.patch[2][2])) / (k + 2)
-            G = np.hypot(gr, gc) # sqrt(x*x + y*y)
-            if G > max_gradient:
-                max_gradient = G
-                max_gradient_vec = np.array([gc, gr])
+        for i in range(self.patch.shape[0]):
+            for j in range(self.patch.shape[1]):
+                point = self.patch[i][j]
+                # gr = ((point.patch[patch_size//2-1][patch_size//2+1] + k * point.patch[patch_size//2][patch_size//2+1] + point.patch[patch_size//2+1][patch_size//2+1]) - (point.patch[patch_size//2-1][patch_size//2-1] + k * point.patch[patch_size//2][patch_size//2-1] + point.patch[patch_size//2+1][patch_size//2-1])) / (k + 2)
+                # gc = ((point.patch[patch_size//2-1][patch_size//2-1] + k * point.patch[patch_size//2-1][patch_size//2] + point.patch[patch_size//2-1][patch_size//2+1]) - (point.patch[patch_size//2+1][patch_size//2-1] + k * point.patch[patch_size//2+1][patch_size//2] + point.patch[patch_size//2+1][patch_size//2+1])) / (k + 2)
+                G = [0 for _ in range(3)]
+                for channel in range(3):
+                    gr = ((point.value_patch[0][2][channel] + k * point.value_patch[1][2][channel] + point.value_patch[2][2][channel]) - (point.value_patch[0][0][channel] + k * point.value_patch[1][0][channel] + point.value_patch[2][0][channel])) / (k + 2)
+                    gc = ((point.value_patch[0][0][channel] + k * point.value_patch[0][1][channel] + point.value_patch[0][2][channel]) - (point.value_patch[2][0][channel] + k * point.value_patch[2][1][channel] + point.value_patch[2][2][channel])) / (k + 2)
+                    G[channel] = np.hypot(gr, gc) # sqrt(x*x + y*y)
+                gradient = sum(G) / 3
+                if gradient > max_gradient:
+                    max_gradient = gradient
+                    max_gradient_vec = np.array([gc, gr])
         magnitude = np.sqrt(max_gradient_vec.dot(max_gradient_vec))
-        gradient_vec = max_gradient_vec / magnitude * max_gradient
+        # gradient_vec = max_gradient_vec / magnitude * max_gradient # normalize?
+        gradient_vec = max_gradient_vec
         return gradient_vec
     
-    def normal_direction(self, contour):
+    def normal_direction(self):
         # theta = np.arctan2(prev_point[0]-next_point[0], prev_point[1]-next_point[1])
         # normal = theta - 0.5 * np.pi
         prev_point = [-1, -1]
-        for i in range(len(contour)):
-            point = contour[i]
-            if contour[i] == [self.r, self.c]:
+        for i in range(len(contour_point)):
+            point = contour_point[i]
+            if contour_point[i] == [self.r, self.c]:
                 if prev_point == [-1, -1]:
-                    prev_point = contour[-1]
-                if i == len(contour)-1:
-                    next_point = contour[0]
+                    prev_point = contour_point[-1]
+                if i == len(contour_point)-1:
+                    next_point = contour_point[0]
                 else:
-                    next_point = contour[i+1]
+                    next_point = contour_point[i+1]
                 break
             prev_point = point
         normal = np.array([prev_point[1]-next_point[1], prev_point[0]-next_point[0]])
         magnitude = np.sqrt(normal.dot(normal))
-        normal = normal / magnitude
+        normal = normal / magnitude * alpha  # normalize?
         return normal
 
 # Tool function
@@ -90,14 +101,27 @@ def init_mask(mask_img): #
 def init_image(img_input, img_mask, patch_size = 3):
     img = np.empty(shape=img_input.shape[0:2], dtype = np.dtype(Pixel))
     first_mask_pixel_xy = [-1, -1]
+    global contour_point
     contour_point = []
     for i in range(img_mask.shape[0]):
         for j in range(img_mask.shape[1]):
-            patch = img_input[max(i-patch_size//2, 0):min(i+1+patch_size//2, img_mask.shape[0]), max(j-patch_size//2, 0):min(j+1+patch_size//2, img_mask.shape[1])]
-            pixel = Pixel(i, j, img_input[i][j], patch, False, img_mask[i][j] == 255) 
+            # value_patch = img_input[max(i-patch_size//2, 0):min(i+1+patch_size//2, img_mask.shape[0]), max(j-patch_size//2, 0):min(j+1+patch_size//2, img_mask.shape[1])]
+            value_patch = np.zeros([3, 3, 3], dtype=np.int8)
+            for i_patch in range(3):
+                for j_patch in range(3):
+                    if i+i_patch-1 > 0 and i+i_patch-1 < img_input.shape[0] and j+j_patch-1 > 0 and j+j_patch-1 < img_input.shape[1]:
+                        value_patch[i_patch][j_patch] = img_input[i+i_patch-1][j+j_patch-1]
+                    else:
+                        value_patch[i_patch][j_patch] = [-1, -1, -1]
+            pixel = Pixel(i, j, img_input[i][j], False, img_mask[i][j] == 255, value_patch) 
             img[i][j] = pixel
             if img_mask[i][j] == 255 and first_mask_pixel_xy == [-1, -1]:
                 first_mask_pixel_xy = [i, j]
+
+    for i in range(img_mask.shape[0]):
+        for j in range(img_mask.shape[1]):
+            patch = img[max(i-patch_size//2, 0):min(i+1+patch_size//2, img_mask.shape[0]), max(j-patch_size//2, 0):min(j+1+patch_size//2, img_mask.shape[1])]
+            img[i][j].set_patch(patch)
 
     now_x, now_y = first_mask_pixel_xy
     contour_point.append(first_mask_pixel_xy)
@@ -128,7 +152,7 @@ def init_image(img_input, img_mask, patch_size = 3):
         # time.sleep(1)
     
     # img = Image(pixels, contour_point)
-    return img, contour_point
+    return img
 
 def find_maxpriority_patch(fillfront):
     max_priority = 0
@@ -169,17 +193,33 @@ def is_fillfront_empty(img):
     
     return True
 
-def generate_result_image_test(img_input, img, contour): # 單純測試有沒有找到欲填範圍的邊緣
+def generate_result_image_test(img_input, img): # 單純測試有沒有找到欲填範圍的邊緣
     img_result = np.zeros(img_input.shape, dtype=np.uint8)
+    # max_magnitude = -1
+    # max_data = -1
     for i in range(img.shape[0]):
         for j in range(img.shape[1]):
-            if [i, j] in contour:
+            if [i, j] in contour_point:
                 img_result[i, j] = [255, 0, 0]
-                print(i, j, img[i][j].normal_direction(contour))
+                norm = img[i][j].normal_direction()
+                gradient = img[i][j].gradient_vector()
+                data = img[i][j].compute_data()
+                print(i, j, norm, gradient, data)
+                # magnitude = np.sqrt(gradient.dot(gradient))
+                # if magnitude > max_magnitude:
+                #     max_magnitude = magnitude
+                #     max_magnitude_vec = gradient
+                # if data > max_data:
+                #     max_data = data
+                #     max_data_norm = norm
+                #     max_data_gradient = gradient
             elif img[i, j].is_fillfront:
                 img_result[i, j] = [255, 255, 255]
             else:
                 img_result[i, j] = img[i, j].value
+    # print(max_magnitude, max_magnitude_vec)
+    # print(max_data, max_data_norm, max_data_gradient)
+    # max_data會到 265.30946553035, norm=[114.03946685 228.0789337 ], gradient=[111.75 240.75]，超過255是正常的嗎？
     return img_result
     
 def generate_result_image(img_input, img):
@@ -200,13 +240,15 @@ def main():
     img_input = cv2.imread(args.input, cv2.IMREAD_COLOR) # 3 channel BGR color image
     img_mask = cv2.imread(args.mask, cv2.IMREAD_GRAYSCALE) # 255~240 and 0~20
     img_mask = init_mask(img_mask) # only 255 and 0, 255 == in fillfront
-    img, contour_point = init_image(img_input, img_mask, args.patch_size) # patch_size is for compute gradient
+    global patch_size
+    patch_size = args.patch_size
+    img = init_image(img_input, img_mask, args.patch_size) # patch_size is for compute gradient
     print(img.shape)
-    img_output = generate_result_image_test(img_input, img, contour_point) # 單純測試有沒有找到欲填範圍的邊緣
+    img_output = generate_result_image_test(img_input, img) # 單純測試有沒有找到欲填範圍的邊緣
 
 
     # 目前完成了找到欲填範圍的邊緣線(演算法有點小漏洞，待修），可以算出邊緣線上點的法向量（np)
-    # 而線性結構方向向量（Ip）可以用單元三在教 Sobel 時算 gradient_magnitude 的部分
+    # 而線性結構方向向量（Ip）可以用單元三在教 Sobel 時算 gradient_vector 的部分
     # 但算 np 和 Ip 還沒寫完 （for compute data）
 
 
