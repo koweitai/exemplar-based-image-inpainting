9dc47292eb62cb3f7d39274d6b6c5706f8aaa376
Kowei <b08902080@ntu.edu.tw>
2023-06-04 23:59:27 +0800

    Generated result12 images

--- RUN.sh
+++ RUN.sh
@@ -14,6 +14,6 @@ fi
 # python3 ImageInpainting.py --input ./dataset/data8/image.png --mask ./dataset/data8/mask.png --output ./result/result8.png --patch_size 9
 # python3 ImageInpainting.py --input ./dataset/data10/image.png --mask ./dataset/data10/mask-2.png --output ./result/test5/result10.png --patch_size 5
 # python3 ImageInpainting.py --input ./dataset/data4/image.jpg --mask ./dataset/data4/mask.jpg --output ./result/result4_test/result4.png --patch_size 7
-python3 ImageInpainting.py --input ./dataset/data11/image.png --mask ./dataset/data11/mask.png --output ./result/result11/result11.png --patch_size 9
+python3 ImageInpainting.py --input ./dataset/data12/image.png --mask ./dataset/data12/mask.png --output ./result/result12/result12.png --patch_size 13
 
 
